2c78a18600aff42a9820d9507be1cf5e0ce73df5
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-14 15:53:29 +0300

    wokrstyled extracted

---
+++ src/components/styled/Work.styled.js
@@ -0,0 +1,171 @@
+import styled from "styled-components";
+import { colors, screen } from "../../../utils";
+
+const textAndBorderColor = (color) => {
+  switch (color) {
+    case colors.violet:
+    case colors.orange:
+      return colors.white;
+    case colors.white:
+      return colors.almostBlack;
+    default:
+      break;
+  }
+};
+
+const gradientColor = (color) => {
+  switch (color) {
+    case colors.violet:
+      return "linear-gradient(90deg, rgba(99, 68, 198, 1) 0%, rgba(82, 96, 115, 1) 100%)";
+    case colors.orange:
+      return "linear-gradient(90deg, rgba(242,100,64, 1) 0%, rgba(82,96,115,1) 100%)";
+    default:
+      return "linear-gradient(90deg, rgba(82, 96, 115, 1) 0%, rgba(256, 256, 256, 1) 100%)";
+  }
+};
+
+const WorkStyled = styled.div`
+  position: relative;
+  min-height: 500px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ bg }) => bg};
+  gap: 1em;
+  padding-top: 2em;
+  padding-bottom: 2em;
+
+  @media (max-width: ${screen.px768}) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 2em;
+    padding-bottom: 2em;
+    gap: 2em;
+  }
+
+  .work-text {
+    color: ${({ bg }) => textAndBorderColor(bg)};
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    @media (max-width: ${screen.px768}) {
+      align-items: flex-start;
+    }
+
+    a {
+      color: ${({ bg }) => textAndBorderColor(bg)};
+    }
+    h2 {
+      margin-bottom: 1em;
+    }
+    ul {
+      display: flex;
+      margin-bottom: 1em;
+      flex-wrap: wrap;
+      li {
+        margin-right: 1em;
+        border: 1px dotted ${({ bg }) => textAndBorderColor(bg)};
+        padding: 0.5em 1em;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 1em;
+      }
+    }
+    button {
+      margin-top: 2em;
+    }
+  }
+
+  .work-img {
+    flex: 1;
+    height: 450px;
+  }
+
+  .work-img__content {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    cursor: pointer;
+
+    img {
+      border: 1px solid ${colors.almostBlack};
+      width: 100%;
+      height: 450px;
+      object-fit: cover;
+      @media (max-width: ${screen.px768}) {
+        max-width: unset;
+        height: auto;
+      }
+    }
+  }
+
+  .overlay {
+    z-index: 1;
+    width: 0;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    transition: all 0.35s ease-in;
+    opacity: 0.8;
+    background: ${({ bg }) => gradientColor(bg)};
+  }
+
+  /* left-right swap positions */
+  .translate-right {
+    transform: translate(50%);
+    position: absolute;
+    width: 45%;
+    margin-left: 4em;
+    align-items: flex-start;
+
+    @media (max-width: ${screen.px768}) {
+      margin-left: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      transform: none;
+      position: relative;
+      width: 100%;
+      text-align: end;
+      .work-text__content {
+        button {
+          margin-left: auto;
+        }
+        ul {
+          justify-content: flex-end;
+        }
+        li {
+          margin-right: 0;
+          margin-left: 1em;
+        }
+      }
+    }
+  }
+
+  .translate-left {
+    position: absolute;
+    width: 45%;
+    transform: translate(-55%);
+    @media (max-width: ${screen.px768}) {
+      transform: none;
+      position: relative;
+      width: 100%;
+    }
+  }
+
+  .case-number {
+    position: absolute;
+    top: 0;
+    z-index: 5;
+    color: white;
+    font-size: 3rem;
+    font-family: "Raleway", sans-serif;
+  }
+`;
+
+
+export default WorkStyled;
