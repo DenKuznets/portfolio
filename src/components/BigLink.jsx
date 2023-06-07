90cf7ad50800f742e3b69d127b12a9c9274bf7ab
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-06-07 11:44:31 +0300

    biglink cleanup

--- src/components/BigLink.jsx
+++ src/components/BigLink.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import { BsArrowUpRight } from "react-icons/bs";
-import { CiLocationArrow1 } from "react-icons/ci";
 import styled from "styled-components";
 import { colors, screen } from "../../utils";
 import { Link } from "react-router-dom";
@@ -53,38 +52,20 @@ const BigLinkStyled = styled(Link)`
 
 const BigLink = (props) => {
     return (
-        <BigLinkStyled
-            className={props.className}
-            onClick={props.onClick}
-            to={props.to}
-        >
+        <BigLinkStyled className={props.className} to={props.to}>
             {props.children}
-            {props.bg === colors.violet ? (
-                <i className="small-icon">
-                    <IconContext.Provider
-                        value={{
-                            size: "1.8em",
-                        }}
-                    >
-                        <div>
-                            <CiLocationArrow1 />
-                        </div>
-                    </IconContext.Provider>
-                </i>
-            ) : (
-                <i className="small-icon">
-                    <IconContext.Provider
-                        value={{
-                            size: "1.2em",
-                            color: colors.almostBlack,
-                        }}
-                    >
-                        <div>
-                            <BsArrowUpRight />
-                        </div>
-                    </IconContext.Provider>
-                </i>
-            )}
+            <i className="small-icon">
+                <IconContext.Provider
+                    value={{
+                        size: "1.2em",
+                        color: colors.almostBlack,
+                    }}
+                >
+                    <div>
+                        <BsArrowUpRight />
+                    </div>
+                </IconContext.Provider>
+            </i>
         </BigLinkStyled>
     );
 };
