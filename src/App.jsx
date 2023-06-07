ee93467b36ad1d54f741e44945c750920334e806
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-06-07 09:54:33 +0300

    app change to router, vite config base removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import {
-} from "react-router-dom";
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
 import MainPage from "./pages/styled/MainPage";
 
 // const router = createHashRouter(
 //     createRoutesFromElements(<Route path="/" element={<MainPage />}></Route>)
 // );
 
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<MainPage/>} />
+))
 function App() {
-    // return <RouterProvider router={router} />;
-    return (
-        <MainPage />
-    )
+    return <RouterProvider router={router} />;
+    
 }
 
 export default App;
